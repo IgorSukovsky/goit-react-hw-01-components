e81438208803b9b04f0ee2c3a676570628f0df96
IgorSukovsky <Sukovsky70@gmail.com>
2021-06-09 18:26:25 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Profile from './components/Profile/Profile';
-import user from './components/Profile/user.json';
-import Statistics from './components/Statistics/Statistics';
-import statisticalData from './components/Statistics/statistical-data.json';
-import FriendList from './components/FriendList/FriendList';
-import friends from './components/FriendList/friends.json';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import transactions from './components/TransactionHistory/transactions.json';
+import Profile from "./components/Profile/Profile";
+import user from "./data/user.json";
+import Statistics from "./components/Statistics/Statistics";
+import statisticalData from "./data/statistical-data.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./data/friends.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./data/transactions.json";
 
 function App() {
   return (
